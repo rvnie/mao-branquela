bf30de0642ca3e58a29e5aeee83cd9a2af61e1c5
rvnie <162621810+rvnie@users.noreply.github.com>
2025-09-18 18:23:28 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-document.getElementById("texto").innerHTML = "Meu primeiro texto <b>JS</b>";
+document.getElementById("texto").innerHTML = 
+"Meu primeiro texto <b>JS</b>";
 console.log("Oi isso é um console.log")
+
+
+
+
+let a,b,c;
+a=5;
+b=6;
+c=7;
+let pessoa = "Dimitri" ;
+let pesso="Dimitri";
+
+
+
+
+function minhafuncao(){
+    document.write(5+5);
+    document.write(5+5);
+    document.write(5+5);
+    document.write(5+5);
+
+
+}
